06cd21432beaaa8691eeacfbc93ae6e225d194eb
Gabriela <gmartinezmenegatty@danielcastelao.org>
2026-09-11 13:59:55 +0200

    Primera Practica

---
+++ Practica 1 Ficheros/src/Metodos.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Metodos {
+
+    public String eDirectorio(String cadea) {
+        File directorio = new File(cadea);
+        if (directorio.isDirectory()) {
+            return "é un directorio";
+        }
+        else {
+            return "non é un directorio";
+        }
+    }
+
+}
+    public String eFicheiro(String cadea) {
+    File fichero = new File(cadea);
+    if (fichero.isFile()) {
+        return "é un ficheiro";
+    }
+    else {
+        return "non é un ficheiro";
+    }
+}
+
+    public boolean creaDirectorio(String ruta) {
+        File fichero = new File(ruta);
+        if (fichero.exists()) {
+            return true;
+        }
+        else {
+            return fichero.mkdir();
+        }
+}
+
+void main() {
+}
+
+
