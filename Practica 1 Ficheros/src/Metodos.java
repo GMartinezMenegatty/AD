d6714b864038e7e9430ce6ed09b4ef59adcecd9b
Gabriela <gmartinezmenegatty@danielcastelao.org>
2026-09-16 22:21:01 +0200

    Practica 1 AD (casi bien)

--- Practica 1 Ficheros/src/Metodos.java
+++ Practica 1 Ficheros/src/Metodos.java
@@ -1,8 +1,7 @@
-import java.util.*;
-
+import java.io.File;
 
 public class Metodos {
-
+    //    ejercicio 1
     public String eDirectorio(String cadea) {
         File directorio = new File(cadea);
         if (directorio.isDirectory()) {
@@ -12,8 +11,8 @@ public class Metodos {
             return "non é un directorio";
         }
     }
-
 }
+//    ejercicio 2
     public String eFicheiro(String cadea) {
     File fichero = new File(cadea);
     if (fichero.isFile()) {
@@ -24,19 +23,37 @@ public class Metodos {
     }
 }
 
+//  ejercicio 3
     public boolean creaDirectorio(String ruta) {
-        File fichero = new File(ruta);
-        if (fichero.exists()) {
-            return true;
-        }
-        else {
-            return fichero.mkdir();
-        }
+    File fichero = new File(ruta);
+    if (fichero.exists()) {
+        return true;
+    }
+    else {
+        return fichero.mkdir();
+    }
 }
 
+//  ejercicio 4
+    public boolean creaFicheiro(String dirName, String fileName) {
+    File directorio = new File(dirName);
+    File ficheiro = new File(dirName, fileName);
 
-//ejercicio 5
-public boolean modoAcceso(String dirName, String fileName) {
+    if (!directorio.exists() || !directorio.isDirectory()) {
+        return false;
+    }
+    if (ficheiro.exists()) {
+        return true;
+    }
+    try {
+        return ficheiro.createNewFile();
+    } catch (Exception e) {
+        return false;
+    }
+}
+
+//  ejercicio 5
+    public boolean modoAcceso(String dirName, String fileName) {
     File ficheiro = new File(dirName, fileName);
 
     if (ficheiro.canWrite()) {
@@ -53,8 +70,8 @@ public boolean modoAcceso(String dirName, String fileName) {
     return false;
 }
 
-//ejercicio 6
-public static void calculaLonxitude(String dirName, String fileName) {
+//  ejercicio 6
+    public static void calculaLonxitude(String dirName, String fileName) {
     File fichero = new File(dirName, fileName);
 
     if (fichero.isFile()) {
@@ -64,8 +81,76 @@ public static void calculaLonxitude(String dirName, String fileName) {
     }
 }
 
+//  ejercicio 7
+    public void mLectura(String dirName, String fileName) {
+    File ficheiro = new File(dirName, fileName);
+    if (ficheiro.exists()) {
+        ficheiro.setReadOnly();
+    }
+}
+
+//  ejercicio 8
+    public void mEscritura(String dirName, String fileName) {
+    File ficheiro = new File(dirName, fileName);
+    if (ficheiro.exists()) {
+        ficheiro.setWritable(true);
+    }
+}
+
+//  ejercicio 9
+    public void borraFicheiro(String dirName, String fileName) {
+    File ficheiro = new File(dirName, fileName);
+
+    if (ficheiro.exists()) {
+        ficheiro.delete();
+    } else {
+        System.out.println("fichero inexistente");
+    }
+}
+
+//  ejercicio 10
+    public void borraDirectorio(String dirName) {
+    File directorio = new File(dirName);
+
+    if (directorio.exists() && directorio.isDirectory()) {
+        directorio.delete();
+    } else {
+        System.out.println("ruta inexistente ou con descencencia");
+    }
+}
+
+//  ejercicio 11
+    public void mContido(String dirName) {
+    File directorio = new File(dirName);
+
+    if (directorio.exists() && directorio.isDirectory()) {
+        File[] contido = directorio.listFiles();
+        for (File elemento : contido) {
+            System.out.println(elemento.getName());
+        }
+    } else {
+        System.out.println("ruta inexistente");
+    }
+}
+//  ejercicio 12
+    public void recur(File directorio) {
+    if (directorio.exists() && directorio.isDirectory()) {
+        File[] elementos = directorio.listFiles();
+        for (File elemento : elementos) {
+            System.out.println(elemento.getAbsolutePath());
+            if (elemento.isDirectory()) {
+                recur(elemento);
+            }
+        }
+    }
+}
+
 
 void main() {
+}
+
+public static void prueba() {
+    System.out.println("Hola");
 }
 
 
